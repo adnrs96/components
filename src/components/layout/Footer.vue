26a3f29eebd7a35d054609dc7000c6c362af0793
Jean Barriere <jean@barriere.io>
2019-01-29 15:06:37 +0100

    fix(footer): put footer into container

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,35 +1,39 @@
 <template>
-  <section :class="['columns', 'is-desktop', { 'is-stars': dark }, 'footer']">
-    <div class="column">
-      <p class="is-size-8 has-text-uppercase has-text-centered has-text-weight-bold">
-        PASSIONATELY BUILT WITH <a-icon icon="heart" /> IN AMSTERDAM
-      </p>
-    </div>
-    <div class="column">
-      <p class="is-size-8 has-text-uppercase has-text-centered has-spans has-text-weight-bold has-letter-spacing-large">
-        <span>&copy; 2019 ASYNCY, Inc.</span>
-        <a
-          :class="[`has-text-${dark ? 'white' : 'dark'}`]"
-          href="//asyncy.click/slack"
-          title="Asyncy Slack"
-        >
-          <i class="mdi mdi-slack" />
-        </a>
-        <a
-          :class="[`has-text-${dark ? 'white' : 'dark'}`]"
-          href="//twitter.com/asyncy"
-          title="Asyncy Twitter"
-        >
-          <i class="mdi mdi-twitter" />
-        </a>
-        <a
-          :class="[`has-text-${dark ? 'white' : 'dark'}`]"
-          href="//github.com/asyncy"
-          title="Asyncy Github"
-        >
-          <i class="mdi mdi-github-circle" />
-        </a>
-      </p>
+  <section :class="[{ 'is-stars': dark }, 'footer']">
+    <div class="containter">
+      <div class="columns is-desktop">
+        <div class="column">
+          <p class="is-size-8 has-text-uppercase has-text-centered has-text-weight-bold">
+            PASSIONATELY BUILT WITH <a-icon icon="heart" /> IN AMSTERDAM
+          </p>
+        </div>
+        <div class="column">
+          <p class="is-size-8 has-text-uppercase has-text-centered has-spans has-text-weight-bold has-letter-spacing-large">
+            <span>&copy; 2019 ASYNCY, Inc.</span>
+            <a
+              :class="[`has-text-${dark ? 'white' : 'dark'}`]"
+              href="//asyncy.click/slack"
+              title="Asyncy Slack"
+            >
+              <i class="mdi mdi-slack" />
+            </a>
+            <a
+              :class="[`has-text-${dark ? 'white' : 'dark'}`]"
+              href="//twitter.com/asyncy"
+              title="Asyncy Twitter"
+            >
+              <i class="mdi mdi-twitter" />
+            </a>
+            <a
+              :class="[`has-text-${dark ? 'white' : 'dark'}`]"
+              href="//github.com/asyncy"
+              title="Asyncy Github"
+            >
+              <i class="mdi mdi-github-circle" />
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
   </section>
 </template>
