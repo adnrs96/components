dcda4be458daf04675ebc059ef4f7b5bd07467ba
Jean Barriere <jean@barriere.io>
2019-04-26 16:53:27 +0200

    fix(tests): fix logo tests

--- tests/unit/components/elements/Logo.spec.js
+++ tests/unit/components/elements/Logo.spec.js
@@ -1,19 +1,21 @@
-import { shallowMount } from '@vue/test-utils'
+import {
+  shallowMount
+} from '@vue/test-utils'
 import Logo from '@/components/elements/Logo.vue'
 
 describe('Logo.vue', () => {
   it('renders logo as icon properly', () => {
     const wrapper = shallowMount(Logo, {
-      propsData: { icon: true }
+      propsData: {
+        icon: true
+      }
     })
-    expect(wrapper.contains('path#Asyncy_logomark')).toBe(true)
-    expect(wrapper.contains('path#Asyncy_wordmark')).toBe(false)
+    expect(wrapper.contains('g')).toBe(true)
   })
   it('renders logo wordmark without logo items', () => {
     const wrapper = shallowMount(Logo, {
       propsData: {}
     })
-    expect(wrapper.contains('path#Asyncy_logomark')).toBe(false)
-    expect(wrapper.contains('path#Asyncy_wordmark')).toBe(true)
+    expect(wrapper.contains('g')).toBe(false)
   })
 })
