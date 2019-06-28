efeccff88088549052ba1c4409af85d2ca554f41
Jean Barriere <jean@barriere.io>
2019-06-28 17:21:42 +0200

    feat(icon+tooltip): add question-mark icon + fix tooltip style

--- src/components/elements/icons/index.js
+++ src/components/elements/icons/index.js
@@ -8,6 +8,7 @@ import SIconHeart from './Heart.vue'
 import SIconHome from './Home.vue'
 import SIconPaginationNext from './PaginationNext.vue'
 import SIconPen from './Pen.vue'
+import SIconQuestionMark from './QuestionMark.vue'
 import SIconRocket from './Rocket.vue'
 import SIconSearch from './Search.vue'
 import SIconSeparator from './Separator.vue'
@@ -24,6 +25,7 @@ export default {
   SIconPaginationNext,
   SIconPen,
   SIconRocket,
+  SIconQuestionMark,
   SIconSearch,
   SIconSeparator
 }
