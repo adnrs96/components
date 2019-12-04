71cc33018ffe1ee67d3655f0fb213bec36361236
Jean Barriere <jean@barriere.io>
2019-12-04 14:27:02 +0100

    chore(eslint): fix lint

--- types/storyscript.d.ts
+++ types/storyscript.d.ts
@@ -1,7 +1,7 @@
 import { ThemeColorsEnum, AccentColorsEnum } from './theme'
 
 export declare class StoryscriptPlugin {
-  constructor(store: any)
+  constructor (store: any)
   setThemeColor: (color: ThemeColorsEnum) => void
   setAccentColor: (accent: AccentColorsEnum) => void
 }
